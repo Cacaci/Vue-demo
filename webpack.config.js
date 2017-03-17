4ee7dcf30d6b0d2992a3800d9bf482c020fbc433
bonzstars <logoweiyi@sina.cn>
2017-03-18 01:41:29 +0800

    20170318

--- webpack.config.js
+++ webpack.config.js
@@ -11,21 +11,21 @@ module.exports = {
     }
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
+        use: ['babel-loader'],
         exclude: /node_modules/
       },
       {
         test: /\.vue$/,
-        loader: 'vue'
+        use: ['vue-loader']
       }
     ]
-  },
+  }/*,
   babel: {
     presets: ['es2015'],
     // vue中使用mapActions等辅助函数，需要安装transform-object-rest-spread插件
     plugins: ['transform-runtime', 'transform-object-rest-spread']
-  }
+  }*/
 }
